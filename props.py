5c279de5e137292ff614e394540824261dadd3d6
Zehra Kaya <72206348+itszehrakaya@users.noreply.github.com>
2023-03-05 16:03:26 +0300

    Create props.py

---
+++ props.py
@@ -0,0 +1,17 @@
+props = {
+  "icon": "IconSource",
+  "mode": "IconButtonMode",
+  "iconColor": "string",
+  "containerColor": "string",
+  "selected": "boolean",
+  "size": "number",
+  "disabled": "boolean",
+  "accessibilityLabel": "string",
+  "onPress": "(e: GestureResponderEvent) => void",
+  "style": "StyleProp<ViewStyle>",
+  "ref": "React.RefObject<TouchableWithoutFeedback>",
+  "theme": "Theme"
+}
+
+for key, value in props.items():
+    print(f"### {key}","\n", f"Type: `{value}`","\n")
